f11bb51b61ca24b33f4dcde6118f211117f1c28d
josh.c <junyu.live@gmail.com>
2016-10-07 16:37:29 -0700

    Update configuration code structue.

--- Configurations/Configurations.swift
+++ Configurations/Configurations.swift
@@ -8,42 +8,42 @@
 import Foundation
 
 open class Configuration: NSObject {
-
-    let ConfigurationPlistKey = "ConfigurationFileName"
-    let CurrentConfigurationPlistKey = "Configuration"
+  
+  let ConfigurationPlistKey = "ConfigurationFileName"
+  let CurrentConfigurationPlistKey = "Configuration"
+  
+  open fileprivate(set) var configurationName: String?
+  fileprivate var dictionary: NSDictionary!
+  
+  open static func defaultConfiguration() -> Configuration {
+    struct Static {
+      static var instance = Configuration()
+    }
+    return Static.instance
+  }
+  
+  fileprivate override init() {
+    super.init()
     
-    open fileprivate(set) var configurationName: String?
-    fileprivate var dictionary: NSDictionary!
+    let bundle = Bundle.main
     
-    open static func defaultConfiguration() -> Configuration {
-        struct Static {
-            static var instance = Configuration()
-        }
-        return Static.instance
+    self.configurationName = bundle.infoDictionary![CurrentConfigurationPlistKey] as? String
+    
+    guard self.configurationName != nil else {
+      fatalError("No Configuration property found in plist")
     }
     
-    fileprivate override init() {
-        super.init()
-
-        let bundle = Bundle.main
-
-        self.configurationName = bundle.infoDictionary![CurrentConfigurationPlistKey] as? String
-        
-        guard self.configurationName != nil else {
-            fatalError("No Configuration property found in plist")
-        }
-        
-        let plistName = bundle.infoDictionary![self.ConfigurationPlistKey] as! String
-        let plistPath = bundle.path(forResource: plistName, ofType: "plist")
-        let dictionary = NSDictionary(contentsOfFile: plistPath!)
-        
-        self.dictionary = dictionary?.value(forKey: self.configurationName!) as! NSDictionary
-    }
+    let plistName = bundle.infoDictionary![self.ConfigurationPlistKey] as! String
+    let plistPath = bundle.path(forResource: plistName, ofType: "plist")
+    let dictionary = NSDictionary(contentsOfFile: plistPath!)
     
-    open subscript(key: String) -> AnyObject? {
-        get {
-            return self.dictionary.value(forKey: key) as AnyObject?
-        }
+    self.dictionary = dictionary?.value(forKey: self.configurationName!) as! NSDictionary
+  }
+  
+  open subscript(key: String) -> AnyObject? {
+    get {
+      return self.dictionary.value(forKey: key) as AnyObject?
     }
-
+  }
+  
 }
